f77f695dd575b788580952b749255130346444e9
Ezeoke Chukwunonso <ezeoke95@gmail.com>
2018-04-20 15:14:24 +0100

    [156810304] Complete initial templates

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,40 +1,97 @@
 // Modal
-const modal = document.getElementById('myModal');
+const modal = document.getElementById('modal');
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+const btn = document.getElementsByClassName('js-modal-btn');
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const close = document.getElementsByClassName('close')[0];
 
 // Table rows
 const trows = document.querySelectorAll('.table tbody tr');
 
-// When the user clicks on the button, open the modal
-if (btn) {
-btn.onclick = function() {
-    modal.style.display = "block";
-  }
+const cards = document.getElementsByClassName('js-card--baseline');
+
+const order = document.getElementsByClassName('js-add-order');
+
+const removeOrder = document.getElementsByClassName('js-remove-order');
+
+const orders = document.getElementsByClassName('js-orders');
+
+for (let card of cards) {
+  card.addEventListener('mouseover', function(evt) {
+    let actionBar = this.querySelector(' .card__body__detail__action');
+
+    actionBar.style.transition = 'height 0.3s ease-in-out';
+
+    actionBar.style.height = '30px';
+
+    let elements = actionBar.querySelectorAll('[aria-hidden]');
+
+    actionBar.querySelector(' div').style.opacity = 1;
+
+    Array.prototype.forEach.call(elements, element => {
+      element.attributes['aria-hidden'] = false;
+    });
+  });
+
+  card.addEventListener('mouseout', function(evt) {
+    let actionBar = this.querySelector(' .card__body__detail__action');
+
+    actionBar.style.transition = 'height 0.1s linear';
+    actionBar.style.height = '4px';
+
+    actionBar.querySelector(' div').style.opacity = 0;
+
+    let elements = actionBar.querySelectorAll('[aria-hidden]');
+
+    actionBar.querySelector(' div').style.opacity = 0;
+
+
+    Array.prototype.forEach.call(elements, element => {
+      element.attributes['aria-hidden'] = true;
+    });
+  });
 }
 
-// When the user clicks on <span> (x), close the modal
-if (span) {
-span.onclick = function() {
-    modal.style.display = "none";
-}
+for (let i of order) {
+  i.addEventListener('click', function() {
+    this.parentNode.parentNode.parentNode.querySelector(' .js-orders').textContent++;
+  })
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+
+for (let button of btn) {
+  button.addEventListener('click', showModal);
+}
+
+close.addEventListener('click', closeModal);
+
+function showModal (evt) {
+  modal.style.display = 'block';
+
+  // hide body scrollbar
+  document.body.style.overflow = 'hidden';
+
+  window.addEventListener('click', closeModal);
+}
+
+function closeModal(evt) {
+
+  if (evt.target == close || evt.target == modal) {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+
+    if (evt.target == modal) {
+      window.removeEventListener('click', closeModal);
     }
+  }
+
 }
+
 
 // activate first tr element
-const first = document.querySelector('.table tbody tr:first-child');
-
-first.classList.add('active');
+// const first = document.querySelector('.table tbody tr:first-child');
+//
+// first.classList.add('active');
 
 // two ways of looping through a nodelist that work
 Array.prototype.forEach.call(trows, tr => {
