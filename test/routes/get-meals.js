666a6ffb00cf2df7cff6dea0d63030f281be3678
Ezeoke Chukwunonso <ezeoke95@gmail.com>
2018-04-29 13:10:55 +0100

    Add pull request template

--- test/routes/get-meals.js
+++ test/routes/get-meals.js
@@ -4,8 +4,8 @@ import {
   },
 } from '../../server/helpers';
 
-suite('Logged in user can get all the meals in the application', function() {
-  test('Expect response to contain all the meals in the application', function() {
+suite('Logged in user can get all the meals in the application', function () {
+  test('Expect response to contain all the meals in the application', function () {
     request
     .get(`/api/${V}meals`)
     .then((res) => ({
