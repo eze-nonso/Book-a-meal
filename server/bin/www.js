a22319bc12b7da5d4004dc84e9b85d98c7a1cccf
Ezeoke Chukwunonso <ezeoke95@gmail.com>
2018-05-04 11:12:41 +0100

    Correct schema error

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,7 +1,17 @@
 import app from '../app';
+import { umzug } from '../models';
 
-const port = parseInt(process.env.PORT, 10) || 8000;
 
-app.set('port', port);
-
-app.listen(app.get('port'));
+Promise.resolve()
+  .then(() => {
+    // run pending migrations for envs other than test
+    return process.env.NODE_ENV === 'test'
+    ? undefined
+    : umzug.up()
+  })
+  .then(() => {
+    return app.set('port', parseInt(process.env.PORT, 10) || 8000);
+  })
+  .then(() => {
+    return app.listen(app.get('port'));
+  });
